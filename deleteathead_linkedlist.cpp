8b1c7bc68d1df5dd0d6c52749672dc40a6fb5ca9
dp221201 <65181405+dp221201@users.noreply.github.com>
2021-08-19 08:36:51 +0530

    Add files via upload

---
+++ deleteathead_linkedlist.cpp
@@ -0,0 +1,55 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+class node{
+    public:
+    int data;
+    node* next;
+    node(int val){
+        data=val;
+        next=NULL;
+
+    }
+};
+void insertAtTail(node* &head,int val){
+    node* n=new node(val);
+    if(head=NULL){
+        head=n;
+        return;
+
+    }
+    node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+
+    }
+    temp->next=n;
+}
+void display(node* head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+void insertathead(node* &head,int val){
+    node* n=new node(val);
+    n->next=head;
+    head=n;
+    
+}
+void deleteAtHead(node* &head){
+    node* todelete=head;
+    head=head->next;
+    delete todelete;
+    
+}
+int main(){
+
+}
