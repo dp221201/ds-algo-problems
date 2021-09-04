5bb5c9c38ebb37baaddd874eb5983c9c469c7559
dp221201 <65181405+dp221201@users.noreply.github.com>
2021-09-04 12:36:21 +0530

    Add files via upload

---
+++ complex-number-multiplication.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+#include<string.h>
+
+class Solution {
+public:
+    string complexNumberMultiply(string a, string b) {
+        string n1,n2,c1,c2;
+        int i=0;
+        while(a[i]!='+')
+            n1+=a[i++];
+            c1=a.substr(i+1,a.size()-i-2);
+        int i=0;
+        while(b[i]!='+')
+            n2+=b[i++];
+            c2=b.substr(i+1,b.size()-i-2);
+            int n = stoi(n1), m = stoi(n2);
+        int c = stoi(c1), d = stoi(c2);
+        string ans = to_string(n * m - c * d) + "+" + to_string(m * c + n * d) + "i";
+        return ans;        
+    
+        
+        
+    }
+};
+int main(){
+
+   return 0;
+}
